5f928735550cc0d2a087b1e4bd780da2f36dabaf
David-Rodriguez-cabrera <alum.david.rodriguez.cabrera@ieslosremedios.org>
2021-11-12 12:56:31 +0100

    1 Subida

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyCIqreWp092-KyHjEms4SzSdU7T0gi8hmI",
+    authDomain: "ejemplo-cloud-firestore-56054.firebaseapp.com",
+    projectId: "ejemplo-cloud-firestore-56054",
+    storageBucket: "ejemplo-cloud-firestore-56054.appspot.com",
+    messagingSenderId: "283028650645",
+    appId: "1:283028650645:web:e35e87399858c9c0d9799c"
+  }
 };
 
 /*
