8a6557c6cffe8d99aa5fabc08e5fd93d746412fe
David-Rodriguez-cabrera <alum.david.rodriguez.cabrera@ieslosremedios.org>
2021-11-16 14:53:18 +0100

    Subida-Acabado-sin-modificaciones-propias

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,9 +11,19 @@ export class HomePage {
 
   tareaEditando: Tarea;
 
+  arrayColeccionTareas: any = [{
+    id: "",
+    data: {} as Tarea
+   }];
+
+  idTareaSelec: string;
+
+  
+
   constructor(private firestoreService: FirestoreService) {
     //Crear una tarea vacía al empezar
     this.tareaEditando = {} as Tarea;
+    this.obtenerListaTareas();
   }
   clickBotonInsertar() {
     this.firestoreService.insertar("tareas", this.tareaEditando)
@@ -26,4 +36,44 @@ export class HomePage {
     });
 
   }
+
+  obtenerListaTareas(){
+    this.firestoreService.consultar("tareas").subscribe((resultadoConsultaTareas) => {
+      this.arrayColeccionTareas = [];
+      resultadoConsultaTareas.forEach((datosTarea: any) => {
+        this.arrayColeccionTareas.push({
+          id: datosTarea.payload.doc.id,
+          data: datosTarea.payload.doc.data()
+        });
+      })
+    });
+  }
+
+  selecTarea(tareaSelec) {
+    console.log("Tarea seleccionada: ");
+    console.log(tareaSelec);
+    this.idTareaSelec = tareaSelec.id;
+    this.tareaEditando.titulo = tareaSelec.data.titulo;
+    this.tareaEditando.descripcion = tareaSelec.data.descripcion;
+  }
+
+  clicBotonBorrar() {
+    this.firestoreService.borrar("tareas", this.idTareaSelec).then(() => {
+      // Actualizar la lista completa
+      this.obtenerListaTareas();
+      // Limpiar datos de pantalla
+      this.tareaEditando = {} as Tarea;
+    })
+  }
+
+  clicBotonModificar() {
+    this.firestoreService.actualizar("tareas", this.idTareaSelec, this.tareaEditando).then(() => {
+      // Actualizar la lista completa
+      this.obtenerListaTareas();
+      // Limpiar datos de pantalla
+      this.tareaEditando = {} as Tarea;
+    })
+  }
+
+  
 }
