8ac31b1e1b3b7bd8940ba177669f9aa90f32b099
David-Rodriguez-cabrera <alum.david.rodriguez.cabrera@ieslosremedios.org>
2022-02-25 13:05:33 +0100

    mapa sin centrar

--- src/app/informacion/informacion.page.ts
+++ src/app/informacion/informacion.page.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { CallNumber } from '@awesome-cordova-plugins/call-number/ngx';
+import * as L from 'leaflet';
 @Component({
   selector: 'app-informacion',
   templateUrl: './informacion.page.html',
   styleUrls: ['./informacion.page.scss'],
 })
 export class InformacionPage implements OnInit {
-
+  map: L.Map;
   constructor(private callNumber: CallNumber) { }
 
   ngOnInit() {
@@ -16,6 +17,18 @@ export class InformacionPage implements OnInit {
 .then(res => console.log('Launched dialer!', res))
 .catch(err => console.log('Error launching dialer', err));
  
+}
+ionViewDidEnter(){
+  this.loadMap();
+}
+
+loadMap() {
+  let latitud = 36.6797047;
+  let longitud = -5.4470656;
+  let zoom = 30;
+  this.map = L.map("mapId").setView([latitud, longitud], zoom);
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
+      .addTo(this.map);
 }
 
 }
