99ec32165ae217e65eace3da668d263410bd408d
David-Rodriguez-cabrera <alum.david.rodriguez.cabrera@ieslosremedios.org>
2022-03-04 14:52:39 +0100

    problema mapa

--- src/app/informacion/informacion.page.ts
+++ src/app/informacion/informacion.page.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { CallNumber } from '@awesome-cordova-plugins/call-number/ngx';
-import * as L from 'leaflet';
+var L = require('leaflet');
+
+var Routing = require('leaflet-routing-machine');
 @Component({
   selector: 'app-informacion',
   templateUrl: './informacion.page.html',
   styleUrls: ['./informacion.page.scss'],
 })
 export class InformacionPage implements OnInit {
-  map: L.Map;
+  map: any;
   constructor(private callNumber: CallNumber) { }
 
   ngOnInit() {
@@ -23,12 +25,23 @@ ionViewDidEnter(){
 }
 
 loadMap() {
+  this.map = L.map("mapId");
   let latitud = 36.679735;
   let longitud = -5.4450258;
-  let zoom = 30;
-  this.map = L.map("mapId").setView([latitud, longitud], zoom);
+  let zoom = 20;
+  this.map.setView([latitud, longitud], zoom);
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
       .addTo(this.map);
+
+      L.Routing.control({
+        waypoints: [
+          L.latLng(36.6752207, -5.4459883),
+          L.latLng(36.679735, -5.4450258)
+        ]
+      }).addTo(this.map);
+    L.marker([36.6752207, -5.4459883]).addTo(this.map).bindPopup('Mi ubicación').openPopup();
+    L.marker([36.679735, -5.4450258]).addTo(this.map).bindPopup('Ubicación Empresa').openPopup();
+    
 }
 
 }
